c5bd05bb9c733f5fa59aacc6c0de62f38ee53262
Josh Tanner <mindful.jt@gmail.com>
2023-05-10 16:49:22 +0900

    bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="shiba-model",
-    version="0.1.0",
+    version="0.1.1",
     author="Octanove Labs",
     author_email="mindful.jt@gmail.com",
     description="An efficient character-level transformer encoder, pretrained for Japanese",
